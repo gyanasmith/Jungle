b3045e89b4906efa36f858e0a2848191a89aeaf2
gyanasmith <156000599+gyanasmith@users.noreply.github.com>
2024-11-21 17:21:51 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+    function animateBird(){
+        $("#bird").animate(
+            {left: '100px',
+            top: '200px',
+            height: '20px'}
+        , 2000);
+    }
+}); //End Document Ready
